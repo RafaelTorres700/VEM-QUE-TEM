a50e1a97af527ad28b0e772497ec3c3b434a30e7
RafaelTorres700 <torres91032@gmail.com>
2025-06-29 16:06:10 -0300

    Criada a pasta layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
-import { FooterComponent } from "./pages/footer/footer.component";
+import { FooterComponent } from "../layout/footer/footer.component";
 import { CreateAcontComponent } from "./pages/create-acont/create-acont.component";
 import { CardsUsuariosComponent } from "./pages/cards-usuarios/cards-usuarios.component";
 import { CardsEstabelecimentosComponent } from "./pages/cards-estabelecimentos/cards-estabelecimentos.component";
-import { HeaderComponent } from './pages/header/header.component';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { HeaderComponent } from '../layout/header/header.component';
+import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,26 @@ import { Routes } from '@angular/router';
 import { CardsEstabelecimentosComponent } from './pages/cards-estabelecimentos/cards-estabelecimentos.component';
 import { CardsUsuariosComponent } from './pages/cards-usuarios/cards-usuarios.component';
 import { CreateAcontComponent } from './pages/create-acont/create-acont.component';
-import { FooterComponent } from './pages/footer/footer.component';
+import { FooterComponent } from '../layout/footer/footer.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { HeaderComponent } from '../layout/header/header.component';
+import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 
 
 export const routes: Routes = [
-  {
+    {
         path: "",
         component: HomeComponent
     },
 
 
-   {
+    {
         path: "loginpage",
         component: LoginPageComponent
     },
 
- {
+    {
         path: "cardsestabelecimentos",
         component: CardsEstabelecimentosComponent
     },
@@ -33,12 +33,12 @@ export const routes: Routes = [
         path: "criarconta",
         component: CreateAcontComponent
     },
- {
+    {
         path: "footer",
         component: FooterComponent
     },
 
-{
+    {
         path: "header",
         component: HeaderComponent
     },
