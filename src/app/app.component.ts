9300e673fa16689259a592ac47ac8d174bf09d9a
RafaelTorres700 <torres91032@gmail.com>
2025-06-30 21:50:27 -0300

    ajustes dia 30/06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeComponent } from "./pages/home/home.component";
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LoginPageComponent } from './formularios/login-page/login-page.component';
 import { FooterComponent } from "../layout/footer/footer.component";
-import { CreateAcontComponent } from "./pages/create-acont/create-acont.component";
+import { CreateAcontComponent } from "./formularios/create-acont/create-acont.component";
 import { CardsUsuariosComponent } from "./pages/cards-usuarios/cards-usuarios.component";
 import { CardsEstabelecimentosComponent } from "./pages/cards-estabelecimentos/cards-estabelecimentos.component";
 import { HeaderComponent } from '../layout/header/header.component';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
-import { CriarEventosComponent } from "./pages/criar-eventos/criar-eventos.component";
+import { CriarEventosComponent } from "./formularios/criar-eventos/criar-eventos.component";
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { CardsEstabelecimentosComponent } from './pages/cards-estabelecimentos/cards-estabelecimentos.component';
 import { CardsUsuariosComponent } from './pages/cards-usuarios/cards-usuarios.component';
-import { CreateAcontComponent } from './pages/create-acont/create-acont.component';
+import { CreateAcontComponent } from './formularios/create-acont/create-acont.component';
 import { FooterComponent } from '../layout/footer/footer.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { LoginPageComponent } from './formularios/login-page/login-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from '../layout/header/header.component';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
-import { CriarEventosComponent } from './pages/criar-eventos/criar-eventos.component';
+import { CriarEventosComponent } from './formularios/criar-eventos/criar-eventos.component';
 
 export const routes: Routes = [
     {
@@ -48,9 +48,9 @@ export const routes: Routes = [
     },
 
 
-{
+    {
         path: "criar-evento",
         component: CriarEventosComponent
-}
+    }
 
 ];
